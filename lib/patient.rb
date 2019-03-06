1be1497fd135794c1ea503f76ee1ee93b6b7f1a1
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-06 23:54:34 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,7 @@ def self.all
 end 
 
 def new_appointment(doctor, date)
- appointment = Appointment.new(doctor, date self)
+ appointment = Appointment.new(doctor, self, date)
 end 
 
 def appointments 
