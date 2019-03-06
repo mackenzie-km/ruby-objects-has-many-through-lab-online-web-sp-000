144d9f7738d32a0c96e1fbf863948648cb8f51aa
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-06 22:34:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,3 +2,11 @@ class Artist
   #DIRECT: relationship with song 
   #INDIRECT: relationship with genre through song 
   
+ attr_reader :name
+ 
+ def initialize(name, artist, genre)
+   @name = name 
+   @artist = artist 
+   @genre = genre 
+   @@all << self 
+ end 
