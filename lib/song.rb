d47c723500a685c00b272ede9573d716fdf65553
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-06 22:31:37 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,7 @@
+def Song 
+  #direct: songs have a direct relationship with the artist and a direct relationship with the genre
+  #indirect: by nature, and to simply everything, songs are connecting points between genres and artists. 
+  
+@@all = []
+
+end 
