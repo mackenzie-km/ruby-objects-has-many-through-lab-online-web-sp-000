36f11ac74bd38b0c131499784ef331b9b918ae12
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-06 23:15:26 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,8 +1,18 @@
 class Doctor 
-attr_accessor :name 
+attr_accessor :name
+
+@@all = [] 
 
 def initialize(name)
   @name = name 
+  @@all << self 
+end 
+
+def new_appointment(date, patient)
+  Appointment.new(date, self, patient)
+end 
+
+def appointments 
 end 
 
 end 
