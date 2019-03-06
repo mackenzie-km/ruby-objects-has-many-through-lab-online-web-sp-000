be88c780e3fefde23f5c43064c691cc2053ef581
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-06 23:05:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,8 +12,7 @@ class Artist
  end 
  
  def new_song(name, genre)
-   artist_name = self.name 
-   name = Song.new(name, artist_name, genre)
+   name = Song.new(name, self, genre)
  end 
  
  def songs
