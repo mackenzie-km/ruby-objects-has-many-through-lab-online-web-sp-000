3ddc9e066dd8867bdb499c203322847b86581379
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-06 22:33:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,19 @@
 def Song 
   #direct: songs have a direct relationship with the artist and a direct relationship with the genre
   #indirect: by nature, and to simply everything, songs are connecting points between genres and artists. 
+ attr_reader :name, :artist, :genre 
+ 
+ def initialize(name, artist, genre)
+   @name = name 
+   @artist = artist 
+   @genre = genre 
+   @@all << self 
+ end 
   
 @@all = []
 
 def self.all
-  @all
+  @@all
 end 
 
 end 
