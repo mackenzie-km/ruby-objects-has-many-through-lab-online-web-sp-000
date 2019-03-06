495576c798e6455a4af59d8329efba29b4de38f3
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-06 23:05:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,9 +12,10 @@ class Artist
  end 
  
  def new_song(name, genre)
-   name = Song.new(name, self, genre)
+   artist_name = self.name 
+   name = Song.new(name, artist_name, genre)
  end 
-   binding.pry 
+ 
  def songs
    Song.all.select do |song| 
      song.artist == self
